ad76e02611e1b0c84211961307a622d603c25c37
Tomoki Kyotani <tkyotani121@gmail.com>
2019-07-10 14:26:05 +0900

    Fix validation error in request_application model

--- app/models/request_application.rb
+++ app/models/request_application.rb
@@ -3,25 +3,7 @@ class Request_Application < ApplicationRecord
     belongs_to :applied_request, class_name: "Request", foreign_key: "request_id"
 
     validates(:applicant_id, uniqueness: {scope: :request_id})
-    #validate(:numOf_working_requests_cannot_be_more_than_recruitment_numbers)
-    #validate(:creator_cannot_apply_to_his_own_request)
-
-    
-    def numOf_working_requests_cannot_be_more_than_recruitment_numbers
-        counter = 0
-
-        applications = Request_Application.where(request_id: self.request_id)
-
-        applications.each { |application|
-            if (application.status == "working")
-                counter += 1
-            end
-
-            if (counter > application.recruitment_number)
-                errors.add(:status, ": 募集人数を超えた採用はできません")
-            end
-        }
-    end
+    validate(:creator_cannot_apply_to_his_own_request)
 
     def creator_cannot_apply_to_his_own_request
         if (self.applicant_id == Request.find(self.request_id).creator_id)
